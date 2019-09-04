e5ac985e985aa4a06597fa8ecf3d5cebe99b2b3f
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-04 00:35:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,23 @@ end
 def pop_with_args(array)
   removed = array.pop(2)
 end
+
+def using_shift(array)
+  removed = array.shift 
+end
+
+def shift_with_args(array)
+  removed = array.shift(2)
+end
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+def using_insert(array,element)
+  array[4] = element
+end
+
+def using_uniq(array)
+  array.uniq
+end
